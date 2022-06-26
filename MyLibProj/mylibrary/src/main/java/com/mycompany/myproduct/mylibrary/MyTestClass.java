479b21059c605fd9ef8f99b889b47fea52f4770f
Velikiy Prikalel <jispar@turbanov.ru>
2022-06-26 12:53:26 +0300

    Add toast functionality to java library.

--- MyLibProj/mylibrary/src/main/java/com/mycompany/myproduct/mylibrary/MyTestClass.java
+++ MyLibProj/mylibrary/src/main/java/com/mycompany/myproduct/mylibrary/MyTestClass.java
@@ -1,11 +1,15 @@
 package com.mycompany.myproduct.mylibrary;
 
-import android.util.Log;
+import android.widget.Toast;
+import android.app.Activity;
 
 public class MyTestClass
 {
-    public static void DebugString ()
-    {
-        Log.d("LibManager", "LOTS OF DEBUG");
+    public static void showSimple(final Activity a, final String toast) {
+        a.runOnUiThread(new Runnable() {
+            public void run() {
+                Toast.makeText(a.getApplicationContext(), toast, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
